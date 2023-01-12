83d92b10e850c61a4ab01cb7a50f3031407f4b12
Ginamarie Sefcik <ginamaries77@gmail.com>
2023-01-11 21:49:03 -0700

    Finished Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,30 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {   
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        int guessed = -1;
+
+        while (guessed != number)
+        {
+            Console.Write("Please Guess a number between 1 & 100: ");
+            guessed = int.Parse(Console.ReadLine());
+
+            if (number > guessed)
+            {
+                Console.WriteLine("Too low, try a higher number");
+            }
+            else if (number < guessed)
+            {
+                Console.WriteLine("Too high, try a lower number");
+            }
+            else
+            {
+                Console.WriteLine("Amazing! You guessed it!");
+            }
+
+        }                    
     }
 }
