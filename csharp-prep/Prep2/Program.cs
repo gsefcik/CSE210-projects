d85b0855cae1b0a75c73a37db2c3ac7db0e3ea5b
Ginamarie Sefcik <ginamaries77@gmail.com>
2023-01-11 20:40:19 -0700

    Finished Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+        Console.Write("Please enter your grade percentage: ");
+        string gpa = Console.ReadLine();
+
+        int grade = int.Parse(gpa);
+        
+        if (grade >= 90)
+        {
+            Console.WriteLine ("You earned an A");
+            Console.WriteLine ("***Congratulations!! You have passed the course!!***");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine ("You earned an B");
+            Console.WriteLine ("***Congratulations!! You have passed the course!!***");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine ("You earned an C");
+            Console.WriteLine ("***Congratulations!! You have passed the course!!***");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine ("You earned an D");
+            Console.WriteLine ("Unfortunately you did not meet the requirements to pass this class. Please try again");
+        }
+        else if (grade >= 59)
+        {
+            Console.WriteLine ("You earned an F");
+            Console.WriteLine ("Unfortunately you did not meet the requirements to pass this class. Please try again");
+        }
+
+            }
 }
